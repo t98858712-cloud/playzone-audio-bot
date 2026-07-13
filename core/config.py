22620c7c5eb323775b06c6f62fb93b7ecb1462c9
t98858712-cloud <t98858712@gmail.com>
2026-07-13 16:06:14 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -35,6 +35,8 @@ FACEBOOK_PLAYZONE = "https://www.facebook.com/share/18goJYQebr/?mibextid=wwXIfr"
 INSTAGRAM_PLAYZONE = "https://www.instagram.com/p1ay.zone?igsh=MW9uYTB1dTZxZnpocQ%3D%3D&utm_source=qr"
 THREADS_PLAYZONE = "https://www.threads.com/@p1ay.zone?igshid=NTc4MTIwNjQ2YQ=="
 TELEGRAM_BOT_PLAYZONE = f"https://t.me/{BOT_USERNAME.replace('@', '')}"
-
 # 🌟 الرابط المباشر الفعلي المعتمد بعد التفعيل من منصة HilltopAds
 HILLTOPADS_LINK = "https://bony-teaching.com/TwZD7z"
+
+# 🌟 الرابط المباشر الفعلي المستخرج من منصة Adsterra (تم تحديثه برابطك الفعلي)
+ADSTERRA_LINK = "https://www.effectivecpmnetwork.com/jgv39bh2p?key=8ffb7ed8cb605d90c6d07e1f7a698646"
