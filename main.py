c2e1aa8634485219abaf3e3561bae2cfa6ad200d
t98858712-cloud <t98858712@gmail.com>
2026-07-17 00:46:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def main():
     app.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, handle_incoming_text))
     app.add_handler(CallbackQueryHandler(handle_callbacks))
 
-    logger.info("⏳ انتظار 5 ثوانٍ قبل بدء تشغيل البوت...")
+    logger.info("⏳ انتظار 10 ثوانٍ قبل بدء تشغيل البوت...")
     time.sleep(10)
     logger.info("🚀 تم تشغيل البوت بنظام الإدارة المؤسسية (Enterprise Control Center) بنجاح.")
     app.run_polling(allowed_updates=Update.ALL_TYPES, drop_pending_updates=True)
