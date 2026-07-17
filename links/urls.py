b8920db6314ffdcac55cf40121c3e0281289ec5c
t98858712-cloud <t98858712@gmail.com>
2026-07-17 23:39:49 +0300

    Delete links directory

--- links/urls.py
+++
@@ -1,9 +0,0 @@
-import os
-
-WEBSITE_PLAYZONE = "http://tasmg1.github.io/tasmg/?"
-FACEBOOK_PLAYZONE = "https://www.facebook.com/share/18goJYQebr/?mibextid=wwXIfr"
-INSTAGRAM_PLAYZONE = "https://www.instagram.com/p1ay.zone?igsh=MW9uYTB1dTZxZnpocQ%3D%3D&utm_source=qr"
-THREADS_PLAYZONE = "https://www.threads.com/@p1ay.zone?igshid=NTc4MTIwNjQ2YQ=="
-
-HILLTOPADS_LINK = "https://bony-teaching.com/TwZD7z"
-ADSTERRA_LINK = "https://www.effectivecpmnetwork.com/jgv39bh2p?key=8ffb7ed8cb605d90c6d07e1f7a698646"
