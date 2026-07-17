a917cf64288dc50bcb389d6d6f9677dfe3d051ea
t98858712-cloud <t98858712@gmail.com>
2026-07-17 12:54:13 +0300

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1306,7 +1306,7 @@ async def send_to_telegram(req: TelegramRequest):
         
         dur = int(req.duration) if req.duration else 0
         caption = f"- @{BOT_USERNAME} , {formatTime(dur)}" if dur > 0 else f"- @{BOT_USERNAME}"
-        reply_markup = {"inline_keyboard": [[{"text": "🌟 مشاركة البوت", "url": "https://t.me/share/url?url=https://t.me/MusicPlayZoneBot"}]]}
+        reply_markup = {"inline_keyboard": [[{"text": "🌟 مشاركة البوت", "url": "https://t.me/share/url?url=https://t.me/P1ay_Z0ne_Bot"}]]}
         
         data = {'chat_id': req.chat_id, 'caption': caption, 'reply_markup': json.dumps(reply_markup)}
         if req.is_audio: data.update({'title': req.title, 'performer': req.performer, 'duration': req.duration})
