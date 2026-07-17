0d63b38d2f839c812cd5a23ad684267b94c8ea81
t98858712-cloud <t98858712@gmail.com>
2026-07-17 23:40:52 +0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-from server import app  # استدعاء آمن ونظيف تماماً من ملفك بعد تغيير اسمه
-
-@app.on_event("startup")
-async def launch_bot_background():
-    # تشغيل ملف البوت في عملية منفصلة بالخلفية فور انطلاق خادم الويب
-    subprocess.Popen(["python", "bot.py"])
