80748c9226d6f4f00658fc5b44eb9f343693599e
t98858712-cloud <t98858712@gmail.com>
2026-07-17 23:41:13 +0300

    Delete server.py

--- server.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from web.api import init_web_routes, WEB_DIR
-
-app = FastAPI(title="PlayZone Dashboard")
-app.mount("/files", StaticFiles(directory=WEB_DIR), name="files")
-init_web_routes(app)
