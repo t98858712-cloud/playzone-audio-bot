efa6cd845f57b19fc5050b1ddc2a036da2899dce
t98858712-cloud <t98858712@gmail.com>
2026-07-20 08:25:12 +0300

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -13,7 +13,7 @@ import libsql_experimental as libsql
 # 1. إعدادات البوت والاتصال السحابي
 # -------------------------------------------------------------
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "")
-BOT_USERNAME = "P1ay_Z0ne_Bot"
+BOT_USERNAME = "MusicPlayZoneBot"
 
 # ضع هنا ID القناة الخاصة التي تم إنشاؤها لتخزين الملفات (مثال: -1001234567890)
 STORAGE_CHANNEL_ID = os.getenv("STORAGE_CHANNEL_ID", "-100XXXXXXXXXX") 
